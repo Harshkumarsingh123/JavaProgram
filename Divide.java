e90d6b279e8145748518a34e7200688e1aeae7d7
Harsh Kumar Singh <117033020+Harshkumarsingh123@users.noreply.github.com>
2024-10-25 23:03:46 +0530

    Add files via upload
    Divide

---
+++ Divide.java
@@ -0,0 +1,26 @@
+class Divide 
+{
+    public int divide(int dividend, int divisor)
+    {
+        int result;
+        if(dividend>Integer.MAX_VALUE || dividend<Integer.MIN_VALUE)
+        {
+            result= 0;
+        }
+        else if (dividend == Integer.MIN_VALUE && divisor == -1)
+           result=Integer.MAX_VALUE;
+        else
+        {
+            result=dividend/divisor;
+        }
+        return result;
+    }
+    public static void main(String [] args)
+    {
+        int dividend=0;
+        int divisor=1;
+        Divide obj=new Divide();
+        int result=obj.divide(dividend,divisor);
+        System.out.println(result);
+    }
+}
